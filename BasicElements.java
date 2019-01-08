7787694c9a96f91076d0d3ed3a8850b3052f622a
paradoxasd <marygammad04@gmail.com>
2019-01-08 13:33:32 +0800

    initialize new java

---
+++ BasicElements.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class BasicElements{
+    
+    public static void main(String args[]){
+        JFrame myFrame = new JFrame();
+
+        myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+        myFrame.setSize(900, 600);
+
+    System.out.println("basic java elements");
+
+    }
+}
